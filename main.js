058f5516a444fd27e8f6bbb949aca800ad7b7b64
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-11-12 19:15:50 +0600

    complite 2section

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-gsap.registerPlugin(ScrollTrigger);
+function loco() {
+    gsap.registerPlugin(ScrollTrigger);
 
 // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
@@ -30,4 +31,8 @@ ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
 // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
 ScrollTrigger.refresh();
+
+
+}
+loco( )
 
